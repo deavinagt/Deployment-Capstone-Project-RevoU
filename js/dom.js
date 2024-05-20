f9eaf92e8e9e3117a62696aaf388f5fa38693f2c
Irene Nabila Saputri <irenenabilasaputri01@gmail.com>
2024-05-20 18:00:48 +0700

    sementara

--- js/dom.js
+++ js/dom.js
@@ -1,14 +1,32 @@
-const toggleButton = document.querySelector('.navbar-toggle');
-    const navbarMenu = document.querySelector('.navbar-menu');
+// const toggleButton = document.querySelector('.navbar-toggle');
+//     const navbarMenu = document.querySelector('.navbar-menu');
 
-    toggleButton.addEventListener('click', () => {
-        navbarMenu.classList.toggle('active');
-    });
+//     toggleButton.addEventListener('click', () => {
+//         navbarMenu.classList.toggle('active');
+//     });
 
-    function toggle(){
-        var blur = document.getElementById('blur');
-        blur.classList.toggle('active');
-        var popup = document.getElementById('blur');
-        popup.classList.toggle('active');
-    }
+// Get the modal
+var modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
