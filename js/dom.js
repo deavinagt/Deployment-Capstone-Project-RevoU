a75ce5d97d59b9f80db2e8b57083815a0d577572
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-05-22 21:43:54 +0700

    fix: modal not responsive

--- js/dom.js
+++ js/dom.js
@@ -14,13 +14,15 @@ let btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 
+const closeButton = document.getElementById("btn-close");
+
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeButton.onclick = function() {
   modal.style.display = "none";
 }
 
